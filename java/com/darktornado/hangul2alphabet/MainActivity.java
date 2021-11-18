94cfbbb1971d7130366b1b41084f22278a508092
Dark Tornado <dt3141592@naver.com>
2021-11-18 21:21:21 +0900

    init

---
+++ java/com/darktornado/hangul2alphabet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.darktornado.hangul2alphabet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
