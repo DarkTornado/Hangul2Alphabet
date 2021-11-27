9da7c44c4d4949b7d0e3097fcd96440091697974
Dark Tornado <dt3141592@naver.com>
2021-11-27 19:26:03 +0900

    일단 UI 완성

--- java/com/darktornado/hangul2alphabet/MainActivity.java
+++ java/com/darktornado/hangul2alphabet/MainActivity.java
@@ -2,6 +2,8 @@ package com.darktornado.hangul2alphabet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -22,6 +24,31 @@ public class MainActivity extends Activity {
         EditText txt2 = new EditText(this);
         txt2.setHint("내용 입력...");
         layout.addView(txt2);
+
+        LinearLayout lay2 = new LinearLayout(this);
+        lay2.setWeightSum(2);
+        Button btn1 = new Button(this);
+        btn1.setText("한글로 변환");
+        btn1.setLayoutParams(new LinearLayout.LayoutParams(-1, -2, 1));
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        lay2.addView(btn1);
+        Button btn2 = new Button(this);
+        btn2.setText("알파벳으로 변환");
+        btn2.setLayoutParams(new LinearLayout.LayoutParams(-1, -2, 1));
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        lay2.addView(btn2);
+        layout.addView(lay2);
+
         TextView txt3 = new TextView(this);
         txt3.setText("\n출력 : ");
         txt3.setTextSize(17);
