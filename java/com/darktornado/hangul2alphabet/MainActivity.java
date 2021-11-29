754890fd6a7df79ab248520373a99d4bdc0e7094
Dark Tornado <dt3141592@naver.com>
2021-11-29 19:47:37 +0900

    일단 반쯤 완성

--- java/com/darktornado/hangul2alphabet/MainActivity.java
+++ java/com/darktornado/hangul2alphabet/MainActivity.java
@@ -3,12 +3,16 @@ package com.darktornado.hangul2alphabet;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,12 +22,20 @@ public class MainActivity extends Activity {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(1);
 
+        final WebView web = new WebView(this);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.addJavascriptInterface(new JSLinker(), "android");
+        web.loadUrl("file:///android_asset/index.html");
+
         TextView txt1 = new TextView(this);
+        final EditText txt2 = new EditText(this);
+        TextView txt3 = new TextView(this);
+        final EditText txt4 = new EditText(this);
+
         txt1.setText("입력 : ");
         txt1.setTextSize(17);
         txt1.setTextColor(Color.BLACK);
         layout.addView(txt1);
-        EditText txt2 = new EditText(this);
         txt2.setHint("내용 입력...");
         layout.addView(txt2);
 
@@ -35,7 +47,8 @@ public class MainActivity extends Activity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String input = txt2.getText().toString();
+                web.loadUrl("javascript:doProcess('" + toHangul(input) + "', false);");
             }
         });
         lay2.addView(btn1);
@@ -45,20 +58,21 @@ public class MainActivity extends Activity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String input = txt2.getText().toString();
+                web.loadUrl("javascript:doProcess('" + input + "', true);");
             }
         });
         lay2.addView(btn2);
         layout.addView(lay2);
 
-        TextView txt3 = new TextView(this);
         txt3.setText("\n출력 : ");
         txt3.setTextSize(17);
         txt3.setTextColor(Color.BLACK);
         layout.addView(txt3);
-        EditText txt4 = new EditText(this);
         txt4.setHint("결과물...");
         layout.addView(txt4);
+
+        layout.addView(web);
 
         int pad = dip2px(16);
         layout.setPadding(pad, pad, pad, pad);
@@ -67,7 +81,38 @@ public class MainActivity extends Activity {
         setContentView(scroll);
     }
 
+    private String toAlphabet(String str) {
+        char[] han = "ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔㅁㄴㅇㄹㅎㅗㅓㅏㅣㅋㅌㅊㅍㅠㅜㅡㅃㅉㄸㄲㅆㅒㅖ".toCharArray();
+        char[] alpha = "qwertyuiopasdfghjklzxcvbnmQWERTOP".toCharArray();
+        for (int n = 0; n < alpha.length; n++) {
+            str = str.replace(han[n], alpha[n]);
+        }
+        return str;
+    }
+
+    private String toHangul(String str) {
+        char[] alpha = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM".toCharArray();
+        char[] han = "ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔㅁㄴㅇㄹㅎㅗㅓㅏㅣㅋㅌㅊㅍㅠㅜㅡㅃㅉㄸㄲㅆㅛㅕㅑㅒㅖㅁㄴㅇㄹㅎㅗㅓㅏㅣㅋㅌㅊㅍㅠㅜㅡ".toCharArray();
+        for (int n = 0; n < alpha.length; n++) {
+            str = str.replace(alpha[n], han[n]);
+        }
+        return str;
+    }
+
+    public void onResultReceived(String input, String output, boolean flag) {
+        if (flag) output = toAlphabet(output);
+        Toast.makeText(this, input + "\n" + output, Toast.LENGTH_SHORT).show();
+    }
+
     private int dip2px(int dips) {
         return (int) Math.ceil(dips * getResources().getDisplayMetrics().density);
     }
+
+    private class JSLinker {
+        @JavascriptInterface
+        public void sendData(final String input, final String output, boolean flag) {
+            new Handler().post(() -> onResultReceived(input, output, flag));
+        }
+    }
+
 }
