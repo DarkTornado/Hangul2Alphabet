65fdc511d838e9acb49fcb651d3cb36c4a7b6d89
Dark Tornado <dt3141592@naver.com>
2021-11-30 21:30:21 +0900

    라이선스 정보 화면 추가

---
+++ java/com/darktornado/hangul2alphabet/LicenseActivity.java
@@ -0,0 +1,12 @@
+package com.darktornado.hangul2alphabet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LicenseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
