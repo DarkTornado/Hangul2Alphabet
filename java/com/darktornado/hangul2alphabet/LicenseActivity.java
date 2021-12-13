c13f2906e8686efe17e54a75b9db38c5a7dfd81b
Dark Tornado <dt3141592@naver.com>
2021-12-13 23:50:48 +0900

    여백 추가

--- java/com/darktornado/hangul2alphabet/LicenseActivity.java
+++ java/com/darktornado/hangul2alphabet/LicenseActivity.java
@@ -13,9 +13,15 @@ public class LicenseActivity extends Activity {
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(1);
 
+        int pad = dip2px(16);
+        layout.setPadding(pad, pad, pad, pad);
         ScrollView scroll = new ScrollView(this);
         scroll.addView(layout);
         setContentView(scroll);
+    }
+
+    private int dip2px(int dips) {
+        return (int) Math.ceil(dips * getResources().getDisplayMetrics().density);
     }
 
 }
