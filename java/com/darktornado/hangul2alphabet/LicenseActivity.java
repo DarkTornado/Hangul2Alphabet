1ebc164e33f52f66e3bcb233024c6435c30f468b
Dark Tornado <dt3141592@naver.com>
2021-12-13 23:49:58 +0900

    Update LicenseActivity.java

--- java/com/darktornado/hangul2alphabet/LicenseActivity.java
+++ java/com/darktornado/hangul2alphabet/LicenseActivity.java
@@ -2,11 +2,20 @@ package com.darktornado.hangul2alphabet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class LicenseActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
+
+        ScrollView scroll = new ScrollView(this);
+        scroll.addView(layout);
+        setContentView(scroll);
     }
+
 }
